7072b38f012bb50a9777d71f224058c862d24f02
Gareth <Gareth.kh.lau@gmail.com>
2021-01-19 11:28:36 -0700

    Define S3 env variables

--- config.js
+++ config.js
@@ -3,4 +3,7 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_SECRET_KEY_ID: process.env.AWS_SECRET_KEY_ID,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
 };
